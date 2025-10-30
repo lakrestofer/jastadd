lakrestofer/jastadd

commit e7abc71cc5492205a5801bbd9adcd3d9cd7e47cc

Generate @Source annotations for some inter-type declarations
Supports methods, fields, and custom constructors.  Extends the scope
of @Source accordingly.

Also allows @Source on type declarations, but does not generate
declarations for them yet.

src/jastadd/ast/JragCodeGen.jrag
@@ -292,8 +292,10 @@ aspect JragCodeGen {
   public void TypeDecl.emitMembers(PrintStream out) {
     for (ClassBodyObject obj : classBodyDecls) {
       org.jastadd.jrag.AST.SimpleNode node = obj.node;
-      out.print(obj.modifiers());
-      out.println(docComment(obj));
+      out.print(obj.modifiers()); // 'public', @UserAnnotation etc.
+      TemplateContext tt = templateContext(obj);
+      tt.bind("RawComment", docComment(obj));
+      tt.expand("InterTypeDeclaration.member", out);
       out.print(config().indent);
       StringBuffer buf = new StringBuffer();
       node.jjtAccept(new ClassBodyDeclUnparser(), buf);

src/template/ast/InterTypeDeclaration.tt
@@ -1,0 +1,35 @@
+# Copyright (c) 2013-2024, The JastAdd Team
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Lund University nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Annotations and documentation for intertype declarations
+InterTypeDeclaration.member [[
+$RawComment
+$if(HasDeclaredAt)
+$if(HasAspectName)
+  $include(MemberDecl.generatedAnnotations)
+$endif
+$endif]]

src/jastadd/ast/Comments.jrag
@@ -143,6 +143,17 @@ outer: while (true) {
    * @return generated javadoc comment
    */
   public String TypeDecl.docComment(ClassBodyObject obj) {
+    TemplateContext tt = templateContext(obj);
+    return tt.expand("ASTDecl.docComment");
+  }
+
+  /**
+   * Constructs TemplateContext set up for a </tt>ClassBodyObject</tt>
+   *
+   * @param obj source context to build TemplateContext from
+   * @return <tt>TemplateContext</tt> with bindings that reflect <tt>obj</tt>
+   */
+  public TemplateContext TypeDecl.templateContext(ClassBodyObject obj) {
     JavaDocParser parser = new JavaDocParser();
     TemplateContext tt = templateContext();
     tt.bind("SourceComment", parser.parse(obj.comments));
@@ -151,7 +162,8 @@ outer: while (true) {
     String declaredat = ASTNode.declaredat(obj.getFileName(), obj.getStartLine());
     tt.bind("HasDeclaredAt", declaredat.length() > 0);
     tt.bind("DeclaredAt", declaredat);
-    return tt.expand("ASTDecl.docComment");
+    tt.bind("SourceLocation", ASTNode.sourceLocation(obj.getFileName(), obj.getStartLine()));
+    return tt;
   }
 
   public String TypeDecl.docComment() {

src/jastadd/core/TemplateUtil.jrag
@@ -55,6 +55,7 @@ aspect TemplateUtil {
     loadTemplates(tt, "ast/CopyNode");
     loadTemplates(tt, "ast/ImplicitDeclarations");
     loadTemplates(tt, "ast/InheritedAttributes");
+    loadTemplates(tt, "ast/InterTypeDeclaration");
     loadTemplates(tt, "ast/JJTree");
     loadTemplates(tt, "ast/List");
     loadTemplates(tt, "ast/NodeConstructor");

src/template/ast/ASTNodeAnnotation.tt
@@ -70,7 +70,10 @@ public class ASTNodeAnnotation {
   public enum Kind { SYN, INH, COLL }
 
   @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-  @java.lang.annotation.Target(java.lang.annotation.ElementType.METHOD)
+  @java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD,
+                                java.lang.annotation.ElementType.CONSTRUCTOR,
+                                java.lang.annotation.ElementType.TYPE,
+				java.lang.annotation.ElementType.FIELD})
   @java.lang.annotation.Documented
   public @interface Source {
     String aspect() default "";
@@ -114,4 +117,9 @@ $endif]]
 TokenComponent.generatedAnnotations = [[
 $if(generateAnnotations)
 @ASTNodeAnnotation.Token(name="$Id")
+$endif]]
+
+MemberDecl.generatedAnnotations = [[
+$if(generateAnnotations)
+@ASTNodeAnnotation.Source(aspect="$AspectName", declaredAt="$SourceLocation")
 $endif]]

commit c93bc7aaf89c09564cbdcdf872b7038cb736fed2

Do not generate annotations for (static) initializers. Fix regression introduced in e7abc71.

src/jastadd/ast/JragCodeGen.jrag
@@ -33,6 +33,7 @@ import org.jastadd.jrag.*;
 
 import org.jastadd.jrag.AST.ASTCompilationUnit;
 import org.jastadd.jrag.AST.ASTBlock;
+import org.jastadd.jrag.AST.ASTInitializer;
 
 import org.jastadd.JastAdd;
 
@@ -299,6 +300,9 @@ aspect JragCodeGen {
       out.print(obj.modifiers()); // 'public', @UserAnnotation etc.
       TemplateContext tt = templateContext(obj);
       tt.bind("RawComment", docComment(obj));
+      // Do not generate AST annotations for (static) initializers (illegal Java code)
+      tt.bind("IsInitializerBlock",
+        obj.node.jjtGetNumChildren() == 1 && obj.node.jjtGetChild(0) instanceof ASTInitializer);
       tt.expand("InterTypeDeclaration.member", out);
       out.print(config().indent);
       StringBuffer buf = new StringBuffer();

src/template/ast/InterTypeDeclaration.tt
@@ -30,6 +30,8 @@ InterTypeDeclaration.member [[
 $RawComment
 $if(HasDeclaredAt)
 $if(HasAspectName)
+$if(!IsInitializerBlock)
   $include(MemberDecl.generatedAnnotations)
+$endif
 $endif
 $endif]]

test/tests/annotation/generated/itd03/Test.jrag
@@ -1,0 +1,26 @@
+// Do not generate annotations for (static) initializers since it's illegal Java code
+// .grammar: {Node;}
+// .options: generateAnnotations=yes
+// .result: EXEC_PASS
+
+import static runtime.Test.*;
+
+aspect Test {
+    public class MyTestClass {
+        int x1 = 1;
+        {
+            x1 = 2;
+        }
+        static int x2 = 3;
+        static {
+            x2 = 4;
+        }
+    }
+
+    public class Test {
+        public static void main(String args[]) {
+            MyTestClass my = new MyTestClass();
+            testSame(6, my.x1 + my.x2);
+        }
+    }
+}