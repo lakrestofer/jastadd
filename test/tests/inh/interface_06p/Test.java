4236ff733e01597bf47c6545ae8ce94280daf693
Christoph Reichenbach <creichen@creichen.net>
2025-11-07 21:33:02 +0000

    Updated tests for interface-hosted attributes (syn, inh, coll)

--- test/tests/inh/interface_06p/Test.java
+++
@@ -1,9 +0,0 @@
-import static runtime.Test.*;
-
-public class Test {
-  public static void main(String[] args) {
-    Father father = new Father(new Son(), new Son());
-    testTrue(father.getFirst().attr());
-    testFalse(father.getSecond().attr());
-  }
-}
